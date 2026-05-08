512cbd47a182be51e153a78205fdb6becf86a9ba
Nelson Ahli <ahlinelson04@gmail.com>
2026-05-08 12:58:02 +0700

    add login page logic

--- app/src/main/java/com/example/project1_anmp/View/LogInFragment.kt
+++ app/src/main/java/com/example/project1_anmp/View/LogInFragment.kt
@@ -1,20 +1,39 @@
 package com.example.project1_anmp.View
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.project1_anmp.R
 import com.example.project1_anmp.databinding.FragmentLogInBinding
 
 class LogInFragment : Fragment() {
     private lateinit var binding: FragmentLogInBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLogInBinding.inflate(inflater)
+        binding = FragmentLogInBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLogin.setOnClickListener {
+            val username = binding.txtUsername.text.toString()
+            val password = binding.txtPass.text.toString()
+
+            if (username == "student" && password == "123") {
+                val action = LogInFragmentDirections.actionDashboardFragment()
+                findNavController().navigate(action)
+            } else {
+                Toast.makeText(context, "Username atau password salah!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
